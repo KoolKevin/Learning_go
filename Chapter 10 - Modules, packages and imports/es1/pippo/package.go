b8b3d06c9b519d0475656a55f663a5009516b6e9
kevin <kevinkoltraka1011@gmail.com>
2025-06-22 19:37:28 +0200

    asd

---
+++ Chapter 10 - Modules, packages and imports/es1/pippo/package.go
@@ -0,0 +1,9 @@
+// Questo è il commento che apparirà su pkg.go.dev per il package pippo.
+// Può essere multilinea e spiegare lo scopo del package.
+package pippo
+
+import "fmt"
+
+func Pippo() {
+	fmt.Print("ciao")
+}
